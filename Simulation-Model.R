c208a4504bc3ef1621eebef06d82b7ab9244abe9
Madeline Jarvis-Cross <51466332+MadelineJC@users.noreply.github.com>
2023-06-30 14:19:48 -0700

    Adding .Rproj file and a file to write and run the simulation model in.

---
+++ Simulation-Model.R
@@ -0,0 +1,8 @@
+#### DOC STRING ===========================================
+# This script proposes a model of eDNA degradation over time
+# in a marine environment subject to UV exposure. The model
+# will be used to simulate stochastic time series data (eDNA 
+# concentration over time). 
+#### END DOC STRING =======================================
+
+#### Parameters ####
