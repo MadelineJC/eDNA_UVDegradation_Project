afd1b315c3a286e796e345b9a185c0a017ddfd4c
Madeline Jarvis-Cross <51466332+MadelineJC@users.noreply.github.com>
2023-06-30 17:58:47 -0700

    Trucking along writing various versions of the exponential decay model, wherein rate of decay varies according to some distribution.

--- Simulation-Model.R
+++ Simulation-Model.R
@@ -12,7 +12,8 @@ library(deSolve)
 #### Parameters ####
 # From Strickler et al., 2015, degradation rate under UV-B exposure ranges between 0.05 and 0.25 when UV-B is between 2 and 50 kJ/m2/day
 
-#### Base exponential decay model ####
+#### Exponential decay models ####
+#### ... Base model ####
 ExponentialDecay <- function(t, y, p){
   # Parms
   lambda <- p[1]
@@ -28,15 +29,12 @@ parms <- c(lambda)
 # Initial values
 N0 <- 200
 # Time point solutions
-t <- seq(0.1, 20, 0.1)
+t <- seq(0.1, 40, 0.1)
 # Simulation
 results <- lsoda(N0, t, ExponentialDecay, parms, verbose = FALSE)
 plot(results)
 
-## OR ##
-# N(t) = N0*exp^(-t/tau)
-
-## When lambda ranges between 0.05 and 0.25
+#### ... When lambda ranges between 0.05 and 0.25 ####
 lambda = seq(0.05, 0.25, 0.01)
 # Initial values
 N0 <- 1
@@ -53,4 +51,37 @@ for (i in 2:ncol(df)){
   lines(t, df[ ,i])
 }
 
-#### Model ####
+#### ... When lambda is pulled from a normal distribution ####
+set.seed(123)
+lambdaDraws <- rnorm(n = length(t), mean = mean(lambda), sd = 0.04); max(lambdaDraws); min(lambdaDraws)
+
+hist(lambdaDraws)
+abline(v = mean(lambdaDraws), col = "red")
+
+# Constant lambda
+N_prev <- 1; N_t <- c()
+for (i in 1:length(t)){
+  N_next <- N_prev*exp(-t[i]/0.15)
+  N_t[i] <- N_next
+  N_next <- N_prev
+}
+plot(N_t, type = "l", xlim = c(0, 20))
+
+# Varying lambda
+N_prev <- 1; N_t <- c()
+for (i in 1:length(t)){
+  N_next <- N_prev*exp(-t[i]/lambdaDraws[i])
+  N_t[i] <- N_next
+  N_next <- N_prev
+}
+plot(N_t, type = "l", xlim = c(0, 20))
+
+#### Inconsistent sampling ####
+
+
+
+
+
+
+
+
