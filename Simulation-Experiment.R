954ee50123d6ca3e5aa464a9c07a5e01ed80dbdc
Madeline Jarvis-Cross <51466332+MadelineJC@users.noreply.github.com>
2023-07-14 13:32:24 -0700

    Re-vamping the data generating component.

--- Simulation-Experiment.R
+++ Simulation-Experiment.R
@@ -4,21 +4,22 @@ library(tidyverse)
 
 #### Getting constant degradation rates for each of 9 barrels ####
 # Required objects
-min <- 0.05; max <- 0.25; mean <- (min + max)/2; print(mean)
+min <- 0.05; max <- 0.34; mean <- (min + max)/2; print(mean)
 num_barrels <- 9
 # Taking a look at distribution
 hist(rnorm(1000, mean = mean, sd = 0.1))
-# Sampling 9 lambdas
-lambdas <- c()
+# Sampling 9 decay rates
+r_vec <- c()
 set.seed(123)
 for (i in 1:num_barrels) {
-  lambdas[i] <- round(rnorm(1, mean = mean, sd = 0.01), digits = 2)
-  while(lambdas[i] < min | lambdas[i] > max)
-  {lambdas[i] <- round(rnorm(1, mean = mean, sd = 0.01), digits = 2)}
-}; print(lambdas); print(mean(lambdas))
+  r_vec[i] <- round(rnorm(1, mean = mean, sd = 0.01), digits = 2)
+  while(r_vec[i] < min | r_vec[i] > max)
+  {r_vec[i] <- round(rnorm(1, mean = mean, sd = 0.01), digits = 2)}
+}; print(r_vec); print(mean(r_vec))
 
 #### Setting up sampling regimes for each of 9 barrels ####
-all_times <- seq(0 + 1/24, 10, 1/24); num_samples <- floor(length(all_times)/num_barrels) # Full time series divided by number of barrels; we can change this to make it more realistic; used floor() instead of round() to always round down
+all_times <- seq(0 + 12/24, 60, 12/24); num_samples <- floor(length(all_times)/num_barrels) # Full time series (in days) divided by number of barrels; we can change this to make it more realistic; used floor() instead of round() to always round down
+# all_times <- seq(1, 60, 1); num_samples <- floor(length(all_times)/num_barrels) # DAYS
 ts <- matrix(nrow = num_samples + 1, ncol = num_barrels) # Matrix of sampling regimes wherein each column contains the sampling regime for a different barrel
 ts[1, ] <- rep(0, ncol(ts))
 set.seed(123)
@@ -32,108 +33,19 @@ all_times <- sort(c(0, ts[2:nrow(ts), ]))
 
 #### Observation without error ####
 y0_obs <- 1e6
-y_obs <- y0_obs*exp(-all_times/mean(lambdas))
-plot(y_obs)
+y_obs <- y0_obs*exp(-all_times*mean(r_vec))
+plot(x = all_times, y = y_obs)
+abline(h = y0_obs*0.05)
 
 #### Setting up barrel volumes to implement sampling error ####
-samp_vol <- 5; barrel_vol <- 100
+samp_vol <- 5; barrel_vol <- 150 # Under these conditions, you can sample each barrel a maximum of 40 times
 vol <- c(barrel_vol)
 for (i in 2:length(t1)){
   vol[i] <- vol[i - 1] - samp_vol
 }
 
 #### Generating data ####
-## Barrel 1
-# Setting up initial abundance, and empty vector to fill with observations
-y_prev <- 1e6; y1_obs_err <- c(); y1_obs_err[1] <- y_prev; j <- 1
-# Loop to generate data with (1) process error, (2) environmental stochasticity, and (3) sampling error
-for (i in 2:length(t1)){
-  y_d <- rbinom(1, y_prev, exp(-(t1[i] - t1[i-1])/lambdas[1]*exp(-rnorm(1, 0, 0.1)))) # Determine eDNAs that "die" via exponential distribution (process error) and environmental stochasticity
-  y_select <- rbinom(1, y_prev - y_d, samp_vol/vol[i]) # Sample eDNAs from remaining with sampling error via sampled volume vs total remaining volume
-  y_next <- y_prev - y_d - y_select # eDNAs in barrel at next time step is equal to those available at previous time step, minus those that died, and those that were selected
-  y_prev <- y_next # Re-assigning y_prev for next iteration
-  y1_obs_err[i] <- y_select # Add sampled eDNAs to observation vector
-}
-
-## Barrel 2
-y_prev <- 1e6; y2_obs_err <- c(); y2_obs_err[1] <- y_prev; j <- 1
-for (i in 2:length(t2)){
-  y_d <- rbinom(1, y_prev, exp(-(t2[i] - t2[i-1])/lambdas[2]*exp(-rnorm(1, 0, 0.1)))) 
-  y_select <- rbinom(1, y_prev - y_d, samp_vol/vol[i]) 
-  y_next <- y_prev - y_d - y_select 
-  y_prev <- y_next 
-  y2_obs_err[i] <- y_select 
-}
-
-## Barrel 3
-y_prev <- 1e6; y3_obs_err <- c(); y3_obs_err[1] <- y_prev; j <- 1
-for (i in 2:length(t3)){
-  y_d <- rbinom(1, y_prev, exp(-(t3[i] - t3[i-1])/lambdas[3]*exp(-rnorm(1, 0, 0.1)))) 
-  y_select <- rbinom(1, y_prev - y_d, samp_vol/vol[i]) 
-  y_next <- y_prev - y_d - y_select 
-  y_prev <- y_next 
-  y3_obs_err[i] <- y_select 
-}
-
-## Barrel 4
-y_prev <- 1e6; y4_obs_err <- c(); y4_obs_err[1] <- y_prev; j <- 1
-for (i in 2:length(t4)){
-  y_d <- rbinom(1, y_prev, exp(-(t4[i] - t4[i-1])/lambdas[4]*exp(-rnorm(1, 0, 0.1)))) 
-  y_select <- rbinom(1, y_prev - y_d, samp_vol/vol[i]) 
-  y_next <- y_prev - y_d - y_select 
-  y_prev <- y_next 
-  y4_obs_err[i] <- y_select 
-}
-
-## Barrel 5
-y_prev <- 1e6; y5_obs_err <- c(); y5_obs_err[1] <- y_prev; j <- 1
-for (i in 2:length(t5)){
-  y_d <- rbinom(1, y_prev, exp(-(t5[i] - t5[i-1])/lambdas[5]*exp(-rnorm(1, 0, 0.1)))) 
-  y_select <- rbinom(1, y_prev - y_d, samp_vol/vol[i]) 
-  y_next <- y_prev - y_d - y_select 
-  y_prev <- y_next 
-  y5_obs_err[i] <- y_select 
-}
-
-## Barrel 6
-y_prev <- 1e6; y6_obs_err <- c(); y6_obs_err[1] <- y_prev; j <- 1
-for (i in 2:length(t6)){
-  y_d <- rbinom(1, y_prev, exp(-(t6[i] - t6[i-1])/lambdas[6]*exp(-rnorm(1, 0, 0.1)))) 
-  y_select <- rbinom(1, y_prev - y_d, samp_vol/vol[i]) 
-  y_next <- y_prev - y_d - y_select 
-  y_prev <- y_next 
-  y6_obs_err[i] <- y_select 
-}
-
-## Barrel 7
-y_prev <- 1e6; y7_obs_err <- c(); y7_obs_err[1] <- y_prev; j <- 1
-for (i in 2:length(t7)){
-  y_d <- rbinom(1, y_prev, exp(-(t7[i] - t7[i-1])/lambdas[7]*exp(-rnorm(1, 0, 0.1)))) 
-  y_select <- rbinom(1, y_prev - y_d, samp_vol/vol[i]) 
-  y_next <- y_prev - y_d - y_select 
-  y_prev <- y_next 
-  y7_obs_err[i] <- y_select 
-}
-
-## Barrel 8
-y_prev <- 1e6; y8_obs_err <- c(); y8_obs_err[1] <- y_prev; j <- 1
-for (i in 2:length(t8)){
-  y_d <- rbinom(1, y_prev, exp(-(t8[i] - t8[i-1])/lambdas[8]*exp(-rnorm(1, 0, 0.1)))) 
-  y_select <- rbinom(1, y_prev - y_d, samp_vol/vol[i]) 
-  y_next <- y_prev - y_d - y_select 
-  y_prev <- y_next 
-  y8_obs_err[i] <- y_select 
-}
-
-## Barrel 9
-y_prev <- 1e6; y9_obs_err <- c(); y9_obs_err[1] <- y_prev; j <- 1
-for (i in 2:length(t9)){
-  y_d <- rbinom(1, y_prev, exp(-(t9[i] - t9[i-1])/lambdas[9]*exp(-rnorm(1, 0, 0.1)))) 
-  y_select <- rbinom(1, y_prev - y_d, samp_vol/vol[i]) 
-  y_next <- y_prev - y_d - y_select 
-  y_prev <- y_next 
-  y9_obs_err[i] <- y_select 
-}
+###### PUT SOMETHING HERE ####
 
 # Combining generated data by barrel
 b1 <- cbind(t1, y1_obs_err); b2 <- cbind(t2, y2_obs_err); b3 <- cbind(t3, y3_obs_err); b4 <- cbind(t4, y4_obs_err); b5 <- cbind(t5, y5_obs_err); b6 <- cbind(t6, y6_obs_err); b7 <- cbind(t7, y7_obs_err); b8 <- cbind(t8, y8_obs_err); b9 <- cbind(t9, y9_obs_err)
@@ -162,5 +74,43 @@ plot(all_times[1:20], y_obs[1:20], type = "l", xlab = "Day", ylab = "eDNA Concen
 points(all_times[1:20], y_obs_err[1:20], col = "cornflowerblue", pch = 16)
 points(all_times[1:20], y_est[1:20], col = "firebrick2", pch = 18)
 legend("topright", legend=c("Deterministic data", "Observed data", "Estimated data"),
-       col=c("black", "cornflowerblue", "firebrick2"), lty = c(1, NA, NA), pch = c(NA, 16, 18), cex=0.8)
+       col=c("black", "cornflowerblue", "firebrick2"), lty = c(1, NA, NA), pch = c(NA, 16, 18), cex = 0.8)
+
+
+#### Marty edits ####
+
+# (1) Separate the time structure of the process model from the time structure of the sampling model
+# (2) The process model should have a standardized and constant time step, maybe equal to 1 hr if the degradation rate is in units per hr. 
+  # So the process model should go through full time series, and sampling model should only be applied on select time steps?
+# (3) The random variable for environmental noise should occur at each time step of the process model rather than the sampling model 
+
+## Barrel 1
+# Setting up initial abundance, and empty vector to fill with observations
+t_elap <- 0.5 # Units are in hours so 10 day * 24hrs, for example; CHANGE ALL_TIMES
+y_prev <- 1e6; y1_obs_err <- c(); y1_obs_err[1] <- y_prev
+# Loop to generate data with (1) process error, (2) environmental stochasticity, and (3) sampling error
+for (i in 2:length(all_times)){
+  y_d <- rbinom(1, y_prev, t_elap*exp(-1*r_vec[1]*exp(-rnorm(1, 0, 0.1)))) # Determine eDNAs that "die" via exponential distribution (process error) and environmental stochasticity
+  if (all_times[i] %in% t1){
+    y_select <- rbinom(1, y_prev - y_d, samp_vol/vol[i - 1]) # Sample eDNAs from remaining with sampling error via sampled volume vs total remaining volume
+    y_next <- y_prev - y_d - y_select # eDNAs in barrel at next time step is equal to those available at previous time step, minus those that died, and those that were selected
+    y_prev <- y_next # Re-assigning y_prev for next iteration
+    y1_obs_err[i] <- y_select # Add sampled eDNAs to observation vector
+  } else {
+    y_next <- y_prev - y_d
+    y_prev <- y_next
+    y1_obs_err[i] <- y_prev
+  }
+}; plot(y1_obs_err)
+First0 <- which(y1_obs_err == 0)[1]
+plot(x = all_times[1:First0], y = y1_obs_err[1:First0])
+
+sampled <- which(all_times %in% t1)
+y1_obs_err <- y1_obs_err[sampled]
+
+# Initially, sample every barrel, every hour, until they're empty
+
+
+
+
 
